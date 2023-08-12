d4c83c9e668174732749260408f8c5aaab73521a
braile <braile.c.gawigawen@gmail.com>
2023-08-12 20:34:37 +0800

    update with title and py

--- pages/_document.js
+++ pages/_document.js
@@ -3,9 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>FM Dictionary</title>
+      </Head>
       <body
-        className={`bg-white dark:bg-fmblack-one p-6 md:px-10 max-w-[375px] md:max-w-[689px]  lg:max-w-[736px] mx-auto`}>
+        className={`bg-white dark:bg-fmblack-one p-6 lg:px-0 lg:py-14 md:px-10 max-w-[375px] md:max-w-[689px]  lg:max-w-[736px] mx-auto`}>
         <Main />
         <NextScript />
       </body>

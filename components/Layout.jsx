a19effc2cfd576d9e94565e79201088179cad6f1
braile <braile.c.gawigawen@gmail.com>
2023-08-13 15:54:27 +0800

    remove dup title tag

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -29,10 +29,6 @@ const Layout = ({ children }) => {
         `}
       </style>
 
-      <Head>
-        <title></title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <Nav />
       <div className={``}>{children}</div>
     </>
